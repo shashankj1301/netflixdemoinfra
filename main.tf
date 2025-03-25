de9788fb53d6a690c0a8ad8b8daffb6e8718b58b
Shashank <shashankjakati1301@gmail.com>
2025-03-25 17:25:44 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,10 +4,10 @@ region = "us-east-1"
 
 resource "aws_instance" "one" {
 count = 5
-ami = "ami-0ddc798b3f1a5117e"
+ami = "ami-04aa00acb1165b32a"
 instance_type = "t2.medium"
-key_name = "swikp"
-vpc_security_group_ids = ["sg-05f044979e305302e"]
+key_name = "sample"
+vpc_security_group_ids = ["sg-0968fc65a89aee561"]
 tags = {
 Name = var.instance_names[count.index]
 }
